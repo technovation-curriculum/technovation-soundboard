811293d6a86a6023a848a20c971de5baecb08be4
technovation-curriculum <technovation.curriculum@gmail.com>
2024-09-12 19:13:41 +0000

    original commit of coded soundboard app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def button_click(col):
-    speech_container.audio(speechArr[col], format='audio/mpeg')
+    speechContainer.audio(speechArr[col], format='audio/mpeg')
 
 st.title("Soundboard App")
 st.subheader(
@@ -18,5 +18,5 @@ for c in range(0, len(names)):
     cols[c].image(imgArr[c])
     cols[c].button(names[c], on_click=button_click, args=(c,), use_container_width=True)
 
-speech_container = st.container(border=True)
-speech_container.write('Audio:')
+speechContainer = st.container(border=True)
+speechContainer.write('Audio: ')
