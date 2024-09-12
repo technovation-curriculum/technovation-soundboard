9c4be45422677beb5b2d656e4d84ae7e89e8363b
technovation-curriculum <technovation.curriculum@gmail.com>
2024-09-12 16:28:17 +0000

    initial soundboard app coded

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+def button_click(col):
+    speech_container.audio(speechArr[col], format='audio/mpeg')
+
+st.title("Soundboard App")
+st.subheader(
+    "Click on a person's name to hear a speech by that person"
 )
+
+names = ['Martin Luther King', 'Kamala Harris', 'Ketanji Brown Jackson']
+imgArr = ['assets/mlk.jpg', 'assets/kamalaharris.png', 'assets/brownjackson.jpg']
+speechArr = ['assets/mlk.mp3', 'assets/kamala.mp3', 'assets/brownjackson.mp3']
+
+cols = st.columns(3)
+
+for c in range(0, len(names)):
+    cols[c].image(imgArr[c])
+    cols[c].button(names[c], on_click=button_click, args=(c,), use_container_width=True)
+
+speech_container = st.container(border=True)
+speech_container.write('Audio:')
